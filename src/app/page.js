acd6d8b697bd18e52649da1d5c43e49e250ce9c6
Avryzel <128247566+Avryzel@users.noreply.github.com>
2026-04-24 11:00:22 +0700

    feat: import asset dan landing page dari project sebelumnya

--- src/app/page.js
+++ src/app/page.js
@@ -1,65 +1,38 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+    <div className="relative h-screen w-full">
+
+      <div
+        className="absolute inset-0 bg-[url('/assets/images/backgrounds/bg3.jpg')] bg-cover bg-center bg-no-repeat -z-10"
+      />
+
+      <div className="absolute inset-0 bg-white/40 flex items-center justify-center pt-20">
+
+        <div className="w-[90%] max-w-[1200px] bg-[#DFF9E8] rounded-[30px] p-8 md:p-[60px] flex flex-col md:flex-row items-center justify-between shadow-[0_20px_40px_rgba(0,0,0,0.2)]">
+
+          <div className="max-w-[500px] text-center md:text-left">
+            <h1 className="text-[48px] text-[#14532d] leading-none font-bold">
+              AVSA<br />
+              <span className="text-[60px] text-[#15803d] font-bold">WASTE</span>
+            </h1>
+            <p className="mt-6 mb-8 text-[#444] text-base leading-relaxed">
+              Aplikasi pelaporan sampah real-time untuk warga, petugas, dan pengelola lingkungan
+            </p>
+            <a href="/login" className="inline-block px-10 py-4 bg-[#22C55E] text-white rounded-full text-lg shadow-[0_5px_15px_rgba(34,197,94,0.3)] hover:bg-[#16A34A] transition-all duration-200 hover:shadow-xl hover:scale-105 active:scale-95">
+              Mulai
+            </a>
+          </div>
+
+          <div className="mt-12 md:mt-0 flex justify-center">
+            <img
+              src="/assets/images/icons/sampah4.png"
+              alt="Ilustrasi Sampah"
+              className="w-full max-w-[500px] h-auto object-contain"
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+          </div>
+
         </div>
-      </main>
+      </div>
     </div>
   );
 }
